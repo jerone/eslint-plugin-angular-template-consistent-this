09e5b3c6d1db49ded6aeee533b426bf548b0d789
Jeroen van Warmerdam <jeronevw@hotmail.com>
2024-04-17 16:15:17 +0200

    📄

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,6 @@
 import type { TSESLint } from "@typescript-eslint/utils";
 
+// See explanation in `./src/lib/external/ensure-template-parser.ts` why this typing is needed.
 declare module "@angular-eslint/utils" {
   export function ensureTemplateParser(
     context: Readonly<TSESLint.RuleContext<string, ReadonlyArray<unknown>>>,
